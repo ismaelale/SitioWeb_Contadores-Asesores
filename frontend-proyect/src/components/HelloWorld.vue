698d9cceb7ee6ffeae354d329e64cf0ebcc2e52c
ismaAle <imuozcabrera12@gmail.com>
2026-08-19 21:34:08 -0600

    Refactor HelloWorld component structure and update CSS import syntax

--- frontend-proyect/src/components/HelloWorld.vue
+++ frontend-proyect/src/components/HelloWorld.vue
@@ -1,8 +1,25 @@
 <script setup>
 
-const count = ref(0)
 </script>
 
 <template>
-  
+    <header class="flex justify-between items-center p-4 text-lg border-1 border-indigo-100 border-b-indigo-100">
+        <h1 class="ml-20 text-xl">Contadores y Asesores</h1>
+        
+        <ul class="flex gap-30">
+            <li>
+                <a href="">Servicios</a>
+            </li>
+            <li>
+                <a href="">Nosotros</a>
+            </li>
+            <li>
+                <a href="">Contacto</a>
+            </li>
+        </ul>
+    
+        <div class="mr-10">
+            <button class="cursor-pointer p-2 w-50">Agendar Consulta</button>
+        </div>
+    </header>
 </template>
