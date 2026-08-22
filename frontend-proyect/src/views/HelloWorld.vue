45059e45b4f86a207fb4d3e30ef76ca89916c97c
ismaAle <imuozcabrera12@gmail.com>
2026-08-22 15:45:28 -0600

    Refactor code structure for improved readability and maintainability

--- frontend-proyect/src/views/HelloWorld.vue
+++ frontend-proyect/src/views/HelloWorld.vue
@@ -11,32 +11,53 @@ const toggleMenu = () => {
 
 <template>
     
-    <header class="p-3 relative flex justify-between border-b border-indigo-100">
-      <h1 class="ml-4 mt-1 whitespace-nowrap text-center">Contadores y Asesores</h1>
+    <header class="relative flex justify-between p-3 border-b border-indigo-100">
 
-      <button @click="toggleMenu" class="md:hidden mr-4 text-2xl">☰</button>
+      <h1 class="lg:ml-5 mt-2 md:whitespace-nowrap">Contadores y Asesores</h1>
 
-      <div v-show="isOpenMenu" class="bg-blue-400 fixed inset-0 z-50 flex justify-center flex-col gap-8 items-center md:static md:!flex md:flex-row md:justify-between md:items-center md:bg-transparent md:w-full">
-        
-        <button @click="toggleMenu" class="md:hidden absolute top-2 right-4 text-2xl">✕</button>
-        
-        <ul class="flex flex-col gap-8 md:flex-row md:mx-auto">
+      <button @click="toggleMenu" class="md:hidden text-2xl cursor-pointer">☰</button>
+
+      <div v-show="isOpenMenu" class="fixed inset-0 flex flex-col z-50 items-center justify-center gap-8 bg-blue-400 md:static md:!flex md:justify-between md:flex-row md:items-center md:bg-transparent md:w-full">
+
+        <button @click="toggleMenu" class="md:hidden absolute top-3 right-3 text-2xl cursor-pointer">
+          ✕
+        </button>
+
+        <ul class="flex flex-col gap-7 md:flex-row md:mx-auto lg:gap-20">
           <li>
-            <a href="" @click="isOpenMenu = false">Servicios</a>
+            <a href="#" @click="isOpenMenu = false">Preguntas</a>
           </li>
           <li>
-            <a href="" @click="isOpenMenu = false">Contacto</a>
+            <a href="#" @click="isOpenMenu = false">Servicios</a>
           </li>
           <li>
-            <a href="" @click="isOpenMenu = false">Nosotros</a>
+            <a href="#" @click="isOpenMenu = false">Nosotros</a>
           </li>
         </ul>
 
         <div>
-            <button class="w-34 bg-blue-300 p-1 rounded-lg">Agentar Cita</button>
-          </div>
+          <button class="lg:mr-5 w-35 bg-blue-500 p-2 rounded-lg">Agendar Cita</button>
+        </div>
       </div>
 
     </header>
+
+    <main class="h-screen flex items-center p-7 flex-col">
+
+      <section class="mt-5">
+        <p class="bg-blue-200 p-2 pr-3 pl-3 rounded-xl text-center">ASESORIA FISCAL & ASESORIA CONTABLE</p>
+        
+        <h2 class="text-3xl mt-7">Rigurosidad Contable y Planeación Fiscal Estratégica</h2>
+        <p class="mt-6">Impulsamos el crecimiento de su empresa con una gestión financiera impecable, 
+          total transparencia y cumplimiento normativo estricto bajo el respaldo de González & Asociados.</p>
+        <a class="flex justify-center mt-5 bg-blue-300 p-4 w-full text-lg font-bold rounded-xl cursor-pointer">Nuestros Servicios</a>
+        <img class="mt-7 rounded-xl aspect-2/2" src="@/assets/mesa_ejecutiva.jpg" alt="">
+          
+      </section>
+
+      <section>
+      </section>
+
+    </main>
 
 </template>
